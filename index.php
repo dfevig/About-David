df74931bdd88b13deec2d75ecbc04d7d2f825f6e
Dylan McDonald <dmcdonald21@cnm.edu>
2014-12-20 19:10:31 -0700

    Added additional content about David

--- index.php
+++ index.php
@@ -6,20 +6,21 @@
 	</head>
 		<body bgcolor="#7fffd4">
 			<h1>Here is a web-page about me...<span>David</span>! <a href="http://www.facebook.com"><img src="https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xfa1/v/t1.0-1/c14.14.172.172/s160x160/200288_1783609083436_7189386_n.jpg?oh=2af65484a7ee79311d1d2b80138e6526&oe=552D4554&__gda__=1429093586_4b539e14dde5f4948c6596500b28c7f8"/></a></h1>
-				<p>I was born in Albuquerque and lived here for most of my life. I went to Hawaii to persue my undergrad and lived out there for
+				<p>I was born in Albuquerque and lived here for most of my life. I went to <s>Hawaii</s> Wasilla, Alaska to pursue my undergrad and lived out there for
 					five years.  I then moved to California to complete my Master's and worked an analytical laboratory and in academia.  I coded scripts in python
 					with R for statistical heavy lifting.  Found problem solving and coding as a passion.</p>
 				<ol>
 					<li><h2>Some activities I enjoy:</h2></li>
 						<ul>
 							<li>Soccer</li>
-							<li>Video games</li>
+							<li>Video games, <strong>especially <em>Hello Kitty Online</em></strong></li>
 							<li>Food</li>
 							<li>Sleeping</li>
 							<li>Snow days</li>
 						</ul>
 					<li><h2>Some activities I do not enjoy:</h2></li>
 						<ul>
+							<li>Writing <em>x</em>86 Assembly</li>
 							<li>Country music</li>
 							<li>Early mornings</li>
 							<li>Not playing in the snow</li>

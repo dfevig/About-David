22a2fb1c7894410d3fd07f0ff891b36978534d9e
David Fevig <dafevig@gmail.com>
2015-01-13 08:44:36 -0700

    about-david

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>About David</title>
+		<link rel="stylesheet" type="text/css" href="style.css" />
+	</head>
+		<body bgcolor="#7fffd4">
+			<h1>Here is a web-page about me...<span>David</span>! <a href="http://www.facebook.com"><img src="https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xfa1/v/t1.0-1/c14.14.172.172/s160x160/200288_1783609083436_7189386_n.jpg?oh=2af65484a7ee79311d1d2b80138e6526&oe=552D4554&__gda__=1429093586_4b539e14dde5f4948c6596500b28c7f8"/></a></h1>
+				<p>I was born in Albuquerque and lived here for most of my life. I went to Hawaii to persue my undergrad and lived out there for
+					five years.  I then moved to California to complete my Master's and worked an analytical laboratory and in academia.  I coded scripts in python
+					with R for statistical heavy lifting.  Found problem solving and coding as a passion.</p>
+				<ol>
+					<li><h2>Some activities I enjoy:</h2></li>
+						<ul>
+							<li>Soccer</li>
+							<li>Video games</li>
+							<li>Food</li>
+							<li>Sleeping</li>
+							<li>Snow days</li>
+						</ul>
+					<li><h2>Some activities I do not enjoy:</h2></li>
+						<ul>
+							<li>Country music</li>
+							<li>Early mornings</li>
+							<li>Not playing in the snow</li>
+						</ul>
+				</ol>
+		<table>
+		<thead>
+			<th colspan="3">Picture Table of Ninjas</th>
+			<tr>
+				<td><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvNAIY3NaRrFl4HrVD6wt5LxpLqVjPD0ja1F6XYyEugjjxMUOm" /></td>
+				<td><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvNAIY3NaRrFl4HrVD6wt5LxpLqVjPD0ja1F6XYyEugjjxMUOm" /></td>
+				<td><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvNAIY3NaRrFl4HrVD6wt5LxpLqVjPD0ja1F6XYyEugjjxMUOm" /></td>
+			</tr>
+		</thead>
+		</table>
+		</body>
+</html>
